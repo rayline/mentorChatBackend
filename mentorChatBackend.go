0959263a66b019b7fcdd53c2ba9f7a49dc9123fb
rayline <yrsty@hotmail.com>
2016-05-25 22:23:32 +0800

    minor fixes on token living

--- mentorChatBackend.go
+++ mentorChatBackend.go
@@ -4,10 +4,16 @@ import (
 	"github.com/astaxie/beego"
 	"log"
 	_ "mentorChatBackend/routers"
+	"os"
 )
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	f, err := os.OpenFile("dev/stderr", os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalln("Unable to create log file : ", err)
+	}
+	log.SetOutput(f)
 }
 
 func main() {
--- models/tokens/tokens.go
+++ models/tokens/tokens.go
@@ -15,7 +15,7 @@ var tokenCnt types.TokenID_t = 1
 
 var tokenMutex sync.RWMutex
 
-const MESSAGE_BLOCKING_LIMIT time.Duration = 30 * time.Second
+const TOKEN_SILENT_LIFE time.Duration = 24 * 60 * 60 * time.Second
 
 func init() {
 
@@ -41,7 +41,7 @@ func Get(Id types.TokenID_t) (types.UserID_t, error) {
 	if t, exist = tokenMap[Id]; exist == false {
 		return 0, fmt.Errorf("Token: Token ID %d does not exist", Id)
 	}
-	if t.lastUsed.Add(MESSAGE_BLOCKING_LIMIT).Before(time.Now()) {
+	if t.lastUsed.Add(TOKEN_SILENT_LIFE).Before(time.Now()) {
 		return 0, fmt.Errorf("Token: Token ID %d is expired", Id)
 	}
 	return t.Id, nil
